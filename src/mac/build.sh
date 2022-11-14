e53395f34d7e48539d813c350acf85187a472add
Vlad Svoka <vlad@coronalabs.com>
2022-11-13 21:42:31 -0500

    Initial release (#1)

--- src/mac/build.sh
+++ src/mac/build.sh
@@ -4,12 +4,16 @@ set -e
 
 path=`dirname $0`
 
+(
+	cd "$path"
+	rm -rf build out
+	xcodebuild -project "Plugin.xcodeproj" -configuration Release clean
 
-xcodebuild -project "$path/Plugin.xcodeproj" -configuration Release clean
+	xcodebuild -project "Plugin.xcodeproj" -configuration Release
 
-xcodebuild -project "$path/Plugin.xcodeproj" -configuration Release
-
-OUTPUT="$path/out"
-mkdir -p "$OUTPUT"
-cp ~/Library/Application\ Support/Corona/Simulator/Plugins/plugin_eos.dylib "$OUTPUT"
-lipo ~/Library/Application\ Support/Corona/Simulator/Plugins/libeos_api.dylib -thin x86_64  -output "$OUTPUT/libeos_api.dylib"
+	OUTPUT="out"
+	mkdir -p "$OUTPUT"
+	cp ~/Library/Application\ Support/Corona/Simulator/Plugins/plugin_eos.dylib "$OUTPUT"
+	# lipo ~/Library/Application\ Support/Corona/Simulator/Plugins/libeos_api.dylib -thin x86_64  -output "$OUTPUT/libeos_api.dylib"
+	cp "$path"/../Dependencies/Epic/redistributable_bin/libEOSSDK-Mac-Shipping.dylib "$OUTPUT"
+)
